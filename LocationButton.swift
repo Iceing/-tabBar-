4b88f4872c7f286e14968f63bbf614b32c89c7a6
Iceing <1948049031@qq.com>
2015-11-24 09:59:26 +0800

    实验

---
+++ LocationButton.swift
@@ -0,0 +1,26 @@
+//
+//  LocationButton.swift
+//  封装tabBar
+//
+//  Created by 蔡跃明 on 15/11/12.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+import UIKit
+
+class LocationButton: UIButton {
+
+    
+    var  locations : String!{
+    
+    
+        get{
+        
+        
+            return self.locations
+        }
+        
+    }
+    
+  
+}
